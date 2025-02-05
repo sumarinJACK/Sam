ab2ec072a70c446854427298cb0c443a2e7eea01
thitkorn <thitikorn_28@hotmail.com>
2025-02-05 22:37:21 +0700

    Initial commit

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ def hello_world():
     return 'Hello, Bu 1650703901'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
-    app.run(debug=True)
+    app.run(debug=True , host="0.0.0.0", port=80)
